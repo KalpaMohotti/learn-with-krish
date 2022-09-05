d6793651bf1d9a6fbb18523a8d8b81339536f22d
KalpaMohotti <kalpa752mohotti@gmail.com>
2022-09-06 02:07:26 +0530

    upload_files

--- Find Missing No/app.js
+++ Find Missing No/app.js
@@ -1,44 +1,55 @@
 
-//const a = [21, 25, 29,22, 24,28,27, 26, 20];          // Last Number Missing 30  
-//const a = [21, 25, 29,22, 24,28,30,27, 26];         // First Number Missing 20
-const a = [21, 25, 29,22, 24,28,30,27, 26, 20];       // Middle Number Missing 23
+// //const a = [21, 25, 29,22, 24,28,27, 26, 20];          // Last Number Missing 30  
+// //const a = [21, 25, 29,22, 24,28,30,27, 26];         // First Number Missing 20
+// const a = [21, 25, 29,22, 24,28,30,27, 26, 20];       // Middle Number Missing 23
 
-let start = 20;            // Sequence start number        
-let end = 30;              // Sequence end number
-
-
-console.log(getMissing_no(a, start, end));
-
-// getMissing_no function
-function getMissing_no(arr, start, end) {
-    arr = arr.sort();
-    var n = arr.length;
+// let start = 20;            // Sequence start number        
+// let end = 30;              // Sequence end number
 
 
-    for (var i = 0; i < n; i++) {
+// console.log(getMissing_no(a, start, end));
 
-        let first_no = arr[i];
+// // getMissing_no function
+// function getMissing_no(arr, start, end) {
+//     arr = arr.sort();
+//     var n = arr.length;
 
-        let next_no = arr[i + 1];
 
-        // Check first number missing
-        if (start == arr[0] - 1) {
-            return "First No " + start + " Missing"
+//     for (var i = 0; i < n; i++) {
+
+//         let first_no = arr[i];
+
+//         let next_no = arr[i + 1];
+
+//         // Check first number missing
+//         if (start == arr[0] - 1) {
+//             return "First No " + start + " Missing"
+//         }
+//         // Return missing number
+//         if (next_no - first_no == 2) {
+//             let missingNo=arr[i]+1;
+//             return "Missing No " +missingNo;
+//         }
+
+//         //Check last number missing
+//         if (end == arr[arr.length - 1] + 1) {
+//             return "Last No " + end + " Missing"
+//         }
+
+
+//     }
+
+// }
+console.log(find_max(30));
+
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    for (let num of nums) {
+        if (num > max_num) {
+      
         }
-        // Return missing number
-        if (next_no - first_no == 2) {
-            let missingNo=arr[i]+1;
-            return "Missing No " +missingNo;
-        }
-
-        //Check last number missing
-        if (end == arr[arr.length - 1] + 1) {
-            return "Last No " + end + " Missing"
-        }
-
-
     }
-
+    return max_num;
 }
 
 
